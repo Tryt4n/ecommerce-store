04515139f98d6713464710c4fbb37f9354c83a29
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-08 13:21:43 +0200

    Created formatters functions.

---
+++ src/lib/formatters.ts
@@ -0,0 +1,15 @@
+const CURRENCY_FORMATTER = new Intl.NumberFormat("pl-PL", {
+  currency: "PLN",
+  style: "currency",
+  minimumFractionDigits: 0,
+});
+
+export function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("pl-PL");
+
+export function formatNumber(amount: number) {
+  return NUMBER_FORMATTER.format(amount);
+}
