b5e34f456c6b0da6091480f0060efce9729e4de5
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-09 15:38:40 +0200

    Added heading for screen readers.

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -5,6 +5,8 @@ import { getMostPopularProducts, getNewestProducts } from "@/db/data";
 export default function HomePage() {
   return (
     <>
+      <h1 className="sr-only">Home page</h1>
+
       <ProductsGridSection
         title="Most Popular"
         productsFetcher={getMostPopularProducts}
