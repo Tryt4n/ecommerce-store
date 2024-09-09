70bd8f118f47ae45e9742c34e03d38bd603728c9
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-09 13:42:30 +0200

    Moved function to get product(s) from `adminData.ts`.

--- src/db/data.ts
+++ src/db/data.ts
@@ -27,3 +27,43 @@ export async function getNewestProducts(numberOfProducts: number = 6) {
     console.error(`Can't get newest products. Error: ${error}`);
   }
 }
+
+export async function getProduct(id: string) {
+  try {
+    const product = await db.product.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        name: true,
+        description: true,
+        priceInCents: true,
+        isAvailableForPurchase: true,
+        filePath: true,
+        imagePath: true,
+      },
+    });
+
+    return product;
+  } catch (error) {
+    console.error(`Can't get product. Error: ${error}`);
+  }
+}
+
+export async function getProducts() {
+  try {
+    const products = await db.product.findMany({
+      select: {
+        id: true,
+        name: true,
+        priceInCents: true,
+        isAvailableForPurchase: true,
+        _count: { select: { orders: true } },
+      },
+      orderBy: { name: "asc" },
+    });
+
+    return products;
+  } catch (error) {
+    console.error(`Can't get products. Error: ${error}`);
+  }
+}
