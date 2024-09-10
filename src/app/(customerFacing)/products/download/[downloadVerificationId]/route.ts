898a8c6d75d901797943ba29934b93fa90d2927a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-10 14:36:23 +0200

    Created "GET" request to download product data.

---
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { getDownloadVerification } from "@/app/_actions/download";
+import { downloadProduct } from "@/lib/downloadProduct";
+
+export async function GET(
+  req: NextRequest,
+  {
+    params: { downloadVerificationId },
+  }: { params: { downloadVerificationId: string } }
+) {
+  const data = await getDownloadVerification(downloadVerificationId);
+
+  if (!data) {
+    return NextResponse.redirect(
+      new URL("/products/download/expired", req.url)
+    );
+  }
+
+  return await downloadProduct(data.product);
+}
