47d2cfdaf32abf79e37705ddc75f28bc657fdbe5
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-10 08:13:10 +0200

    Added stripe variables.

--- src/lib/zod/env.ts
+++ src/lib/zod/env.ts
@@ -4,6 +4,8 @@ const envVariables = z.object({
   DATABASE_URL: z.string().min(1),
   ADMIN_USERNAME: z.string().min(1),
   HASHED_ADMIN_PASSWORD: z.string().min(1),
+  NEXT_PUBLIC_STRIPE_PUBLIC_KEY: z.string().min(1),
+  STRIPE_SECRET_KEY: z.string().min(1),
 });
 
 export const parsedEnv = envVariables.parse(process.env);
