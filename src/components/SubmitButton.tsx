b8c3a399cdac829ba76a2618f205eb1cdbc53127
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-11 10:24:52 +0200

    Marked as client component.

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { type ComponentProps } from "react";
 import { useFormStatus } from "react-dom";
 import { Button } from "./ui/button";
 
-export default function SubmitButton() {
+type SubmitButtonProps = {
+  initialText?: string;
+  pendingText?: string;
+  className?: string;
+  size?: ComponentProps<typeof Button>["size"];
+};
+
+export default function SubmitButton({
+  initialText,
+  pendingText,
+  className,
+  size,
+}: SubmitButtonProps) {
   const { pending } = useFormStatus();
 
   return (
     <Button
       type="submit"
+      size={size}
       disabled={pending}
       variant={pending ? "ghost" : "default"}
+      className={className}
     >
-      {pending ? "Saving..." : "Save"}
+      {pending
+        ? pendingText
+          ? pendingText
+          : "Saving..."
+        : initialText
+          ? initialText
+          : "Save"}
     </Button>
   );
 }
