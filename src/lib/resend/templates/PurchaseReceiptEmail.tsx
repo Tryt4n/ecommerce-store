97cc4da7041689a08b3a91059db0784ed2749f7e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-11 10:33:29 +0200

    Created email template for purchase receipt.

---
+++ src/lib/resend/templates/PurchaseReceiptEmail.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import OrderInformation from "./OrderInformation";
+import type { Order, Product } from "../types";
+import type { DownloadVerification } from "@prisma/client";
+
+type PurchaseReceiptEmailProps = {
+  product: Product;
+  order: Order;
+  downloadVerification: DownloadVerification;
+};
+
+// Only for preview purposes
+PurchaseReceiptEmail.PreviewProps = {
+  product: {
+    name: "Product Name",
+    imagePath:
+      "/products/9a4116b0-82e7-46ac-b2a7-2cd2b318af80-05 - Metadata.jpg",
+    description: "Product Description",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerification: {
+    createdAt: new Date(),
+    expiresAt: new Date(),
+    id: crypto.randomUUID(),
+    productId: crypto.randomUUID(),
+  },
+} satisfies PurchaseReceiptEmailProps;
+// Only for preview purposes
+
+export default function PurchaseReceiptEmail({
+  product,
+  order,
+  downloadVerification,
+}: PurchaseReceiptEmailProps) {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt.</Preview>
+      <Tailwind>
+        <Head />
+
+        <Body className="bg-white font-sans">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+
+            <OrderInformation
+              product={product}
+              order={order}
+              downloadVerification={downloadVerification}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
