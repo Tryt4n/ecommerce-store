dad1736305774feece8a4d3a1f34bfb5b8d95958
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-11 10:30:55 +0200

    Created admin users page.

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminPageHeader from "../_components/AdminPageHeader";
+import UsersTable from "./_components/UsersTable";
+
+export default function AdminUsersPage() {
+  return (
+    <>
+      <AdminPageHeader>Customers</AdminPageHeader>
+      <UsersTable />
+    </>
+  );
+}
