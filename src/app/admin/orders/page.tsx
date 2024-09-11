3fcb8f00980b5ff1ccbf0f3f3d0ac94809933f9f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-11 10:27:22 +0200

    Created admin orders page.

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminPageHeader from "../_components/AdminPageHeader";
+import OrdersTable from "./_components/OrdersTable";
+
+export default function AdminOrdersPage() {
+  return (
+    <>
+      <AdminPageHeader>Sales</AdminPageHeader>
+      <OrdersTable />
+    </>
+  );
+}
