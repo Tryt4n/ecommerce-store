3731c0359cd5c40959a2b3354b28a1d8cb6cd4ab
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-12 12:17:11 +0200

    Created dropdown menu for discount code item.

---
+++ src/app/admin/discount-codes/_components/DiscountDropdownMenu.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  deleteDiscountCode,
+  toggleDiscountCodeActive,
+  type getDiscountCodes,
+} from "@/app/_actions/discounts";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import DeleteDropdownItem from "../../_components/DeleteDropdownItem";
+import ActiveToggleDropdownMenuItem from "../../_components/ActiveToggleDropdownMenuItem";
+import { MoreVertical } from "lucide-react";
+
+export default function DiscountDropdownMenu({
+  discount,
+  canDeactivate,
+}: {
+  discount: NonNullable<
+    Awaited<ReturnType<typeof getDiscountCodes>>
+  >["unexpiredDiscountCodes"][number];
+  canDeactivate?: boolean;
+}) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="transition-all hover:text-slate-400">
+        <MoreVertical />
+        <span className="sr-only">Actions</span>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        {canDeactivate && (
+          <>
+            <ActiveToggleDropdownMenuItem
+              id={discount.id}
+              isActive={discount.isActive}
+              promiseFn={toggleDiscountCodeActive}
+            />
+
+            <DropdownMenuSeparator />
+          </>
+        )}
+
+        <DeleteDropdownItem
+          id={discount.id}
+          disabled={discount._count.orders > 0}
+          promiseFn={deleteDiscountCode}
+        />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
