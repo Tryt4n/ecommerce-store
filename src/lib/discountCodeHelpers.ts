207a7ed797ff6fb89dbf81f091ae9a79b2e970e4
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-12 15:29:50 +0200

    Created helper functions for discount codes.

---
+++ src/lib/discountCodeHelpers.ts
@@ -0,0 +1,37 @@
+import db from "@/db/db";
+import type { DiscountCode, Prisma, Product } from "@prisma/client";
+
+export function usableDiscountCodeWhere(productId: Product["id"]) {
+  return {
+    isActive: true,
+    AND: [
+      {
+        OR: [{ allProducts: true }, { products: { some: { id: productId } } }],
+      },
+      {
+        OR: [{ limit: null }, { limit: { gt: db.discountCode.fields.uses } }],
+      },
+      {
+        OR: [{ expiresAt: null }, { expiresAt: { gt: new Date() } }],
+      },
+    ],
+  } satisfies Prisma.DiscountCodeWhereInput;
+}
+
+export function getDiscountedAmount(
+  discountAmount: DiscountCode["discountAmount"],
+  discountType: DiscountCode["discountType"],
+  priceInCents: Product["priceInCents"]
+) {
+  switch (discountType) {
+    case "PERCENTAGE":
+      return Math.max(
+        1,
+        Math.ceil(priceInCents - priceInCents * (discountAmount / 100))
+      );
+    case "FIXED":
+      return Math.max(1, Math.ceil(priceInCents - discountAmount * 100));
+    default:
+      throw new Error(`Invalid discount type ${discountType satisfies never}`);
+  }
+}
