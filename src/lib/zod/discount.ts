bc22b58a80640b78d9521756cdf13aeaf6997779
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-12 12:10:50 +0200

    Created `zod` validation for adding discount code.

---
+++ src/lib/zod/discount.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import { DiscountCodeType } from "@prisma/client";
+
+export const addDiscountSchema = z
+  .object({
+    code: z.string().min(5),
+    discountAmount: z.coerce.number().int().min(1),
+    discountType: z.nativeEnum(DiscountCodeType),
+    allProducts: z.coerce.boolean(),
+    productIds: z.array(z.string()).optional(),
+    expiresAt: z.preprocess(
+      (value) => (value === "" ? undefined : value),
+      z.coerce.date().min(new Date()).optional()
+    ),
+    limit: z.preprocess(
+      (value) => (value === "" ? undefined : value),
+      z.coerce.number().int().min(1).optional()
+    ),
+  })
+  .refine(
+    (data) =>
+      data.discountAmount <= 100 ||
+      data.discountType !== DiscountCodeType.PERCENTAGE,
+    {
+      message: "Percentage discount must be less than or equal to 100%",
+      path: ["discountAmount"],
+    }
+  )
+  .refine((data) => !data.allProducts || data.productIds == null, {
+    message: "Cannot select products when all products is selected",
+    path: ["productIds"],
+  })
+  .refine((data) => data.allProducts || data.productIds != null, {
+    message: "Must select products when all products is not selected",
+    path: ["productIds"],
+  });
