6879df3e9f81c035a08500ff1409e70ae2d3c23c
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-13 15:49:29 +0200

    Divided the page into sections, added headings, added charts.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,35 @@
 import React from "react";
 import AdminDashboardCardList from "./_components/AdminDashboardCardList";
+import AdminDashboardCharts from "./_components/AdminDashboardCharts";
+import { getDashboardData } from "@/db/adminData";
+import { subDays } from "date-fns";
 
-export default function AdminDashboard() {
-  return <AdminDashboardCardList />;
+export default async function AdminDashboard() {
+  const data = await getDashboardData(subDays(new Date(), 6), new Date());
+
+  return (
+    <>
+      <h1 className="sr-only">Admin Dashboard</h1>
+
+      <section>
+        <h2 className="sr-only">Summary</h2>
+
+        {data && (
+          <AdminDashboardCardList
+            data={{
+              productsData: data.productsData,
+              salesData: data.salesData,
+              usersData: data.usersData,
+            }}
+          />
+        )}
+      </section>
+
+      <section className="mt-8">
+        <h2 className="sr-only">Charts</h2>
+
+        {data && <AdminDashboardCharts data={data.chartsData} />}
+      </section>
+    </>
+  );
 }
