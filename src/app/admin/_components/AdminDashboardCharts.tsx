baaafd6e0fc2c25d1f2586d049d73d74bd02d333
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-13 15:53:48 +0200

    Created component with all admin charts.

---
+++ src/app/admin/_components/AdminDashboardCharts.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import DashboardCard from "@/components/DashboardCard";
+import OrdersByDayChart from "./charts/OrdersByDayChart";
+import UsersByDayChart from "./charts/UsersByDayChart";
+import RevenueByProductChart from "./charts/RevenueByProductChart";
+import type { getDashboardData } from "@/db/adminData";
+
+type AdminDashboardChartsProps = {
+  data: NonNullable<Awaited<ReturnType<typeof getDashboardData>>>["chartsData"];
+};
+
+export default function AdminDashboardCharts({
+  data,
+}: AdminDashboardChartsProps) {
+  return (
+    <div className="grid-cols1 grid gap-4 lg:grid-cols-2">
+      <DashboardCard title="Total Sales">
+        <OrdersByDayChart data={data.ordersCreationData} />
+      </DashboardCard>
+
+      <DashboardCard title="New Customers">
+        <UsersByDayChart data={data.usersCreationDates} />
+      </DashboardCard>
+
+      <DashboardCard title="Revenue By Product">
+        <RevenueByProductChart data={data.revenueByProduct} />
+      </DashboardCard>
+    </div>
+  );
+}
