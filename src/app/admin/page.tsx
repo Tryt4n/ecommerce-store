0d19f01a7b3c735283b0d22731b437c5bc7704a1
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-14 11:06:47 +0200

    Added fetching data based on search params.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,10 +2,41 @@ import React from "react";
 import AdminDashboardCardList from "./_components/AdminDashboardCardList";
 import AdminDashboardCharts from "./_components/AdminDashboardCharts";
 import { getDashboardData } from "@/db/adminData/dashboardData";
-import { subDays } from "date-fns";
+import { getRangeOption, RANGE_OPTIONS } from "@/lib/rangeOptions";
 
-export default async function AdminDashboard() {
-  const data = await getDashboardData(subDays(new Date(), 6), new Date());
+type SearchParams = {
+  searchParams: {
+    totalSalesRange?: string;
+    newCustomersRange?: string;
+    revenueByProductRange?: string;
+  };
+};
+
+export default async function AdminDashboard({ searchParams }: SearchParams) {
+  const { totalSalesRange, newCustomersRange, revenueByProductRange } =
+    searchParams;
+
+  const salesDataRange =
+    getRangeOption(totalSalesRange) || RANGE_OPTIONS.last_7_days;
+  const usersDataRange =
+    getRangeOption(newCustomersRange) || RANGE_OPTIONS.last_7_days;
+  const productsDataRange =
+    getRangeOption(revenueByProductRange) || RANGE_OPTIONS.last_7_days;
+
+  const data = await getDashboardData({
+    salesDataRangeData: {
+      createdAfter: salesDataRange.startDate,
+      createdBefore: salesDataRange.endDate,
+    },
+    customersDataRangeData: {
+      createdAfter: usersDataRange.startDate,
+      createdBefore: usersDataRange.endDate,
+    },
+    revenueByProductData: {
+      createdAfter: productsDataRange.startDate,
+      createdBefore: productsDataRange.endDate,
+    },
+  });
 
   return (
     <>
