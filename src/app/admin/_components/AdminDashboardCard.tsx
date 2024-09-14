f012e80cc95a4ff3773ba6176ab5ef2504d246b4
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-14 11:04:18 +0200

    Added dropdown menu.

--- src/app/admin/_components/AdminDashboardCard.tsx
+++ src/app/admin/_components/AdminDashboardCard.tsx
@@ -6,10 +6,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import ChartDataDropdownMenu from "./ChartDataDropdownMenu";
 
 type DashboardCardProps = {
   title: string;
   subtitle?: string;
+  queryKey?: string;
   body?: string;
   className?: ComponentProps<typeof Card>["className"];
   children?: React.ReactElement;
@@ -18,15 +20,23 @@ type DashboardCardProps = {
 export default function AdminDashboardCard({
   title,
   subtitle,
+  queryKey,
   body,
   className,
   children,
 }: DashboardCardProps) {
   return (
     <Card className={className}>
-      <CardHeader>
+      <CardHeader className="relative">
         <CardTitle>{title}</CardTitle>
         {subtitle && <CardDescription>{subtitle}</CardDescription>}
+
+        {queryKey && (
+          <ChartDataDropdownMenu
+            className="absolute right-4 top-2.5"
+            queryKey={queryKey}
+          />
+        )}
       </CardHeader>
 
       <CardContent>
