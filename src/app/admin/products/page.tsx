7f536f6214862f53e24be42799071e77fa2a39f2
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-15 14:27:17 +0200

    Page wrapped in a context provider.

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import AdminProductsContextProvider from "./_contexts/AdminProductsContext";
 import AdminPageHeader from "../_components/AdminPageHeader";
 import ProductsTable from "./_components/ProductsTable";
 import { Button } from "@/components/ui/button";
 
 export default function AdminProductsPage() {
   return (
-    <>
+    <AdminProductsContextProvider>
       <div className="flex items-center justify-between gap-4">
         <AdminPageHeader>Products</AdminPageHeader>
         <Button href="/admin/products/new">Add Product</Button>
       </div>
 
       <ProductsTable />
-    </>
+    </AdminProductsContextProvider>
   );
 }
