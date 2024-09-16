f85e0d724511ce4d120284af3d8893ec34524ea2
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-16 12:52:58 +0200

    Created function with logic from `/app/admin/orders/_actions`.

--- src/db/adminData/orders.ts
+++ src/db/adminData/orders.ts
@@ -3,6 +3,29 @@
 import { notFound } from "next/navigation";
 import db from "../init";
 import type { Order } from "@prisma/client";
+import type { SortingType } from "@/types/sort";
+
+export async function getOrders(
+  orderBy: keyof Order = "createdAt",
+  type: SortingType = "asc"
+) {
+  try {
+    return db.order.findMany({
+      select: {
+        id: true,
+        pricePaidInCents: true,
+        product: true,
+        user: true,
+        discountCode: {
+          select: { code: true, discountType: true, discountAmount: true },
+        },
+      },
+      orderBy: { [orderBy]: type },
+    });
+  } catch (error) {
+    console.error(`Can't get orders. Error: ${error}`);
+  }
+}
 
 export async function deleteOrder(id: Order["id"]) {
   try {
