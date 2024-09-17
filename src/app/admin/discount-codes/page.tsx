8a51636369567ae3c5b2179edff970ee18927ad0
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-17 10:51:07 +0200

    Moved fetching data to admin context and the code related to discount codes has been moved to a newly created component.

--- src/app/admin/discount-codes/page.tsx
+++ src/app/admin/discount-codes/page.tsx
@@ -1,44 +1,18 @@
 import React from "react";
-import { getDiscountCodes } from "@/db/adminData/discountCodes";
 import AdminPageHeader from "../_components/AdminPageHeader";
-import DiscountCodesTable from "./_components/DiscountCodesTable";
+import DiscountCodesSections from "./_layout/DiscountCodesSections";
 import { Button } from "@/components/ui/button";
 
 export default async function AdminDiscountCodesPage() {
-  const discountCodes = await getDiscountCodes();
-
   return (
     <>
       <div className="flex items-center justify-between gap-4">
         <AdminPageHeader>Coupons</AdminPageHeader>
+
         <Button href="/admin/discount-codes/new">Add Coupon</Button>
       </div>
 
-      <section>
-        <h2 className="sr-only">Active Coupons</h2>
-
-        {discountCodes?.unexpiredDiscountCodes ? (
-          <DiscountCodesTable
-            discountCodes={discountCodes.unexpiredDiscountCodes}
-            canDeactivate
-          />
-        ) : (
-          <p>No active coupons</p>
-        )}
-      </section>
-
-      <section className="mt-8">
-        <h2 className="text-xl font-bold">Expired Coupons</h2>
-
-        {discountCodes?.expiredDiscountCodes ? (
-          <DiscountCodesTable
-            discountCodes={discountCodes.expiredDiscountCodes}
-            isInactive
-          />
-        ) : (
-          <p>No expired coupons</p>
-        )}
-      </section>
+      <DiscountCodesSections />
     </>
   );
 }
