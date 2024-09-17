f1cff86d46f06c40fd5f2c8f67ed1ec940ace249
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-17 10:49:29 +0200

    Deleted old comment.

--- src/app/admin/_hooks/useAdminContext.tsx
+++ src/app/admin/_hooks/useAdminContext.tsx
@@ -1,18 +1,6 @@
 import { useContext } from "react";
 import { AdminContext, type ContextType } from "../_context/AdminContext";
 
-// export function useAdminContext<Data extends object[]>() {
-//   const context = useContext(AdminContext) as ContextType<Data>;
-
-//   if (!context) {
-//     throw new Error(
-//       "useAdminContext must be used within a AdminContextProvider"
-//     );
-//   }
-
-//   return context;
-// }
-
 type ReturnTypeOfPromise<T extends (...args: never) => Promise<unknown>> =
   NonNullable<Awaited<ReturnType<T>>>;
 
