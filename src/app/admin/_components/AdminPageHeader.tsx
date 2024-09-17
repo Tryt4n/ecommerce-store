fb964bea40664efd47b3584a1fecd5b354591927
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-17 12:41:48 +0200

    Added date range dropdown menu to header.

--- src/app/admin/_components/AdminPageHeader.tsx
+++ src/app/admin/_components/AdminPageHeader.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import DateRangeDropdownMenu from "./DateRangeDropdownMenu";
 
 export default function AdminPageHeader({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <h1 className="mb-4 text-4xl">{children}</h1>;
+  return (
+    <div className="mb-4 flex gap-4">
+      <h1 className="text-4xl">{children}</h1>
+
+      <DateRangeDropdownMenu queryKey="dateRange" />
+    </div>
+  );
 }
