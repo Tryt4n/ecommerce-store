e70eec473eaf3a843db6eb6d4a599883a04afe1d
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-17 10:51:45 +0200

    Created component related with code discounts with code from `page.tsx`.

---
+++ src/app/admin/discount-codes/_layout/DiscountCodesSections.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import { useAdminContext } from "../../_hooks/useAdminContext";
+import DiscountCodesTable from "../_components/DiscountCodesTable";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
+import type { getDiscountCodes } from "@/db/adminData/discountCodes";
+
+export default function DiscountCodesSections() {
+  const { data: discountCodes } = useAdminContext<typeof getDiscountCodes>();
+
+  if (
+    !discountCodes ||
+    (!discountCodes.expiredDiscountCodes &&
+      !discountCodes.unexpiredDiscountCodes)
+  ) {
+    return <LoadingSpinner size={64} aria-label="Loading coupons..." />;
+  }
+
+  return (
+    <>
+      <section>
+        <h2 className="sr-only">Active Coupons</h2>
+
+        {discountCodes?.unexpiredDiscountCodes ? (
+          <DiscountCodesTable
+            discountCodes={discountCodes.unexpiredDiscountCodes}
+            canDeactivate
+          />
+        ) : (
+          <p>No active coupons</p>
+        )}
+      </section>
+
+      <section className="mt-8">
+        <h2 className="text-xl font-bold">Expired Coupons</h2>
+
+        {discountCodes?.expiredDiscountCodes ? (
+          <DiscountCodesTable
+            discountCodes={discountCodes.expiredDiscountCodes}
+            isInactive
+          />
+        ) : (
+          <p>No expired coupons</p>
+        )}
+      </section>
+    </>
+  );
+}
