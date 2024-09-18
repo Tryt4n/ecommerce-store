8ef8931d5479f465196d8a8006d9f63821f31218
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-18 12:58:21 +0200

    Added date range dropdown menu as an optional component.

--- src/app/admin/_components/AdminPageHeader.tsx
+++ src/app/admin/_components/AdminPageHeader.tsx
@@ -3,14 +3,16 @@ import DateRangeDropdownMenu from "./DateRangeDropdownMenu";
 
 export default function AdminPageHeader({
   children,
+  withDateRange = true,
 }: {
   children: React.ReactNode;
+  withDateRange?: boolean;
 }) {
   return (
     <div className="mb-4 flex gap-4">
       <h1 className="text-4xl">{children}</h1>
 
-      <DateRangeDropdownMenu queryKey="dateRange" />
+      {withDateRange && <DateRangeDropdownMenu queryKey="dateRange" />}
     </div>
   );
 }
