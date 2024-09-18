cc5119c8a4a4906d0f9eda90f12f3c091a124781
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-18 12:57:39 +0200

    Created action to update discount code.

--- src/app/_actions/discounts.ts
+++ src/app/_actions/discounts.ts
@@ -1,8 +1,13 @@
 "use server";
 
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 import { addDiscountSchema } from "@/lib/zod/discount";
-import { createDiscountCode } from "@/db/adminData/discountCodes";
+import {
+  createDiscountCode,
+  getDiscountCode,
+  updateDiscountCode as updateDiscountCodeInDB,
+} from "@/db/adminData/discountCodes";
+import type { DiscountCode } from "@prisma/client";
 
 export async function addDiscountCode(prevState: unknown, formData: FormData) {
   const productIds = formData.getAll("productIds");
@@ -16,7 +21,33 @@ export async function addDiscountCode(prevState: unknown, formData: FormData) {
     return result.error.formErrors.fieldErrors;
   }
 
-  const data = result.data;
+  const data = { ...result.data, code: result.data.code.trim() };
 
   await createDiscountCode(data).then(() => redirect("/admin/discount-codes"));
+}
+
+export async function updateDiscountCode(
+  code: DiscountCode["code"],
+  prevState: unknown,
+  formData: FormData
+) {
+  const productIds = formData.getAll("productIds");
+
+  const result = addDiscountSchema.safeParse({
+    ...Object.fromEntries(formData.entries()),
+    productIds: productIds.length > 0 ? productIds : undefined,
+  });
+
+  if (result.success === false) {
+    return result.error.formErrors.fieldErrors;
+  }
+
+  const data = { ...result.data, code: result.data.code.trim() };
+  const discountCode = await getDiscountCode(code);
+
+  if (discountCode == null) return notFound();
+
+  await updateDiscountCodeInDB(discountCode.id, data).then(() =>
+    redirect("/admin/discount-codes")
+  );
 }
