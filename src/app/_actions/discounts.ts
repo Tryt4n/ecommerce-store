d6b3619cd6482423258c398897b50105a88a3c66
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-19 15:05:48 +0200

    Added logic for handling discount codes categories.

--- src/app/_actions/discounts.ts
+++ src/app/_actions/discounts.ts
@@ -8,22 +8,19 @@ import {
   updateDiscountCode as updateDiscountCodeInDB,
 } from "@/db/adminData/discountCodes";
 import type { DiscountCode } from "@prisma/client";
+import type { FormDataEntries } from "@/types/formData";
 
 export async function addDiscountCode(prevState: unknown, formData: FormData) {
-  const productIds = formData.getAll("productIds");
+  const data = processFormData(formData);
+  const validationResult = validateAndParseData(data);
 
-  const result = addDiscountSchema.safeParse({
-    ...Object.fromEntries(formData.entries()),
-    productIds: productIds.length > 0 ? productIds : undefined,
-  });
-
-  if (result.success === false) {
-    return result.error.formErrors.fieldErrors;
+  if ("error" in validationResult) {
+    return validationResult.error;
   }
 
-  const data = { ...result.data, code: result.data.code.trim() };
-
-  await createDiscountCode(data).then(() => redirect("/admin/discount-codes"));
+  await createDiscountCode(validationResult.data).then(() =>
+    redirect("/admin/discount-codes")
+  );
 }
 
 export async function updateDiscountCode(
@@ -31,23 +28,54 @@ export async function updateDiscountCode(
   prevState: unknown,
   formData: FormData
 ) {
-  const productIds = formData.getAll("productIds");
+  const data = processFormData(formData);
+  const validationResult = validateAndParseData(data);
 
-  const result = addDiscountSchema.safeParse({
-    ...Object.fromEntries(formData.entries()),
-    productIds: productIds.length > 0 ? productIds : undefined,
-  });
-
-  if (result.success === false) {
-    return result.error.formErrors.fieldErrors;
+  if ("error" in validationResult) {
+    return validationResult.error;
   }
 
-  const data = { ...result.data, code: result.data.code.trim() };
   const discountCode = await getDiscountCode(code);
 
   if (discountCode == null) return notFound();
 
-  await updateDiscountCodeInDB(discountCode.id, data).then(() =>
-    redirect("/admin/discount-codes")
+  await updateDiscountCodeInDB(discountCode.id, validationResult.data).then(
+    () => redirect("/admin/discount-codes")
   );
+}
+
+function processFormData(formData: FormData): FormDataEntries {
+  const data: FormDataEntries = Object.fromEntries(formData.entries());
+  if (typeof data.productIds === "string") {
+    data.productIds = data.productIds.split(",");
+  }
+  if (data.allProducts === "on") {
+    data.productIds = [];
+  }
+  if (typeof data.categories === "string" && data.categories !== "") {
+    data.categories = data.categories.split(",");
+  }
+  if ((data.categories as string[]).length > 0) {
+    data.allProducts = "off";
+    data.productIds = [];
+  }
+  if (data.categories === "") {
+    data.categories = [];
+  }
+
+  return data;
+}
+
+function validateAndParseData(data: FormDataEntries) {
+  const result = addDiscountSchema.safeParse({
+    ...data,
+    productIds:
+      (data.productIds as string[]).length > 0 ? data.productIds : undefined,
+  });
+
+  if (result.success === false) {
+    return { error: result.error.formErrors.fieldErrors };
+  }
+
+  return { data: { ...result.data, code: result.data.code.trim() } };
 }
