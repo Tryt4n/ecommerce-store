1f3a07494d3e155b260e08c9ed7851d8afb8eddf
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-19 12:04:29 +0200

    Created functions to get categories and get categories id by names.

---
+++ src/db/userData/categories.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import db from "../init";
+import type { Prisma } from "@prisma/client";
+
+export async function getCategories() {
+  try {
+    const categories = await db.category.findMany({ select: { name: true } });
+
+    return categories.map((category) => category.name);
+  } catch (error) {
+    console.error(`Can't get categories. Error: ${error}`);
+  }
+}
+
+export async function getCategoryIds(
+  tx: Prisma.TransactionClient,
+  categoryNames: string[]
+): Promise<string[]> {
+  return Promise.all(
+    // Find all categories IDs
+    categoryNames.map(async (categoryName: string) => {
+      let category = await tx.category.findUnique({
+        where: { name: categoryName },
+      });
+
+      // Create new category if it doesn't exist
+      if (!category) {
+        category = await tx.category.create({
+          data: { name: categoryName },
+        });
+      }
+
+      return category.id;
+    })
+  );
+}
