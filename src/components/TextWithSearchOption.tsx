c4b54599748e89c9d6e7fff173174fe5e4f9306e
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-23 11:53:59 +0200

    Created component to wrap searching text into `<mark/>` component.

---
+++ src/components/TextWithSearchOption.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
+export default function TextWithSearchOption({ text }: { text: string }) {
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("searchQuery");
+
+  const parts = text.split(new RegExp(`(${searchQuery})`, "gi")); // Split the text into parts, where the search query is a separate part
+
+  return (
+    <>
+      {searchQuery ? (
+        <span>
+          {parts.map((part, index) =>
+            part.toLowerCase() === searchQuery.toLowerCase() ? (
+              <mark key={index} className="bg-[#fc9a23]">
+                {part}
+              </mark>
+            ) : (
+              part
+            )
+          )}
+        </span>
+      ) : (
+        text
+      )}
+    </>
+  );
+}
