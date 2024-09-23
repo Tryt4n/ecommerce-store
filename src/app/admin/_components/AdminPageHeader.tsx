7991947a04ceb6c3a2b0701216f662c8ec46df3f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-23 11:50:26 +0200

    Added search input.

--- src/app/admin/_components/AdminPageHeader.tsx
+++ src/app/admin/_components/AdminPageHeader.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import DateRangeDropdownMenu from "./DateRangeDropdownMenu";
+import SearchInput from "@/components/SearchInput";
+import { Button } from "@/components/ui/button";
 
 export default function AdminPageHeader({
   children,
   withDateRange = true,
+  withLink,
 }: {
   children: React.ReactNode;
   withDateRange?: boolean;
+  withLink?: { href: string; label: string };
 }) {
   return (
-    <div className="mb-4 flex gap-4">
-      <h1 className="text-4xl">{children}</h1>
+    <>
+      <div className="mb-4 flex items-center justify-between gap-4">
+        <h1 className="text-4xl">{children}</h1>
 
-      {withDateRange && <DateRangeDropdownMenu queryKey="dateRange" />}
-    </div>
+        {withLink && <Button href={withLink.href}>{withLink.label}</Button>}
+      </div>
+
+      <div className="my-4 flex flex-row gap-4">
+        {withDateRange && <DateRangeDropdownMenu queryKey="dateRange" />}
+
+        <SearchInput />
+      </div>
+    </>
   );
 }
