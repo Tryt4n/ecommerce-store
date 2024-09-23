32583569fd39c1c2a678d9b1b94a5f91404df9ad
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-23 11:52:11 +0200

    Deleted unnecessary comment.

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,7 +1,6 @@
 import { unstable_cache as nextCache } from "next/cache";
 import { cache as reactCache } from "react";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Callback = (...args: any[]) => Promise<any>;
 
 type Options = {
