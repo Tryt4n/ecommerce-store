ac243f4fa0d811132e13b1f9f1a08793ba875550
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-23 11:51:02 +0200

    Added searching functionality.

--- src/app/admin/_context/AdminContext.tsx
+++ src/app/admin/_context/AdminContext.tsx
@@ -9,6 +9,8 @@ import { getDiscountCodes } from "@/db/adminData/discountCodes";
 import { arraysEqual, sortArray } from "@/lib/sort";
 import { setSortingSearchParams } from "@/lib/searchParams";
 import { getRangeOption, RANGE_OPTIONS } from "@/lib/rangeOptions";
+import { format } from "date-fns";
+import { pl } from "date-fns/locale";
 import type { SortingType } from "@/types/sort";
 import type { DateRange } from "@/types/ranges";
 
@@ -28,7 +30,6 @@ export type ContextType<Data extends object[]> = {
   refetchData: () => void;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const AdminContext = createContext<ContextType<any> | null>(null);
 
 export default function AdminContextProvider<Data extends object[]>({
@@ -48,6 +49,7 @@ export default function AdminContextProvider<Data extends object[]>({
   const dateRange = searchParams.get("dateRange") as undefined | string;
   const dateRangeFrom = searchParams.get("dateRangeFrom") as undefined | string;
   const dateRangeTo = searchParams.get("dateRangeTo") as undefined | string;
+  const searchQuery = searchParams.get("searchQuery") as undefined | string;
 
   const fetchData = useCallback(async () => {
     let fetchedData;
@@ -109,6 +111,109 @@ export default function AdminContextProvider<Data extends object[]>({
       });
     }
 
+    if (searchQuery) {
+      // Define keys to check for each type of data
+      const productsKeysToCheck = ["name", "categories", "priceInCents"];
+      const usersKeysToCheck = ["email", "ordersValue", "ordersCount"];
+      const ordersKeysToCheck = [
+        "createdAt",
+        "productName",
+        "userEmail",
+        "pricePaidInCents",
+        "discountCode",
+      ];
+      const discountCodesKeysToCheck = [
+        "code",
+        "discountAmount",
+        "expiresAt",
+        "products",
+        "categories",
+      ];
+
+      // Combine all keys to check into a single array
+      const keysToCheck = [
+        ...productsKeysToCheck,
+        ...usersKeysToCheck,
+        ...ordersKeysToCheck,
+        ...discountCodesKeysToCheck,
+      ];
+
+      // Function to filter data based on search query
+      const filterData = (data: Record<string, any>[]) => {
+        return data.filter((item) => {
+          // Extract values for each key from the item
+          const values = keysToCheck.map((key) => item[key]);
+
+          // Check if any of the values match the search query
+          return values.some((value) => {
+            if (typeof value === "string") {
+              // Check if string value includes the search query
+              return value.toLowerCase().includes(searchQuery.toLowerCase());
+            } else if (typeof value === "number") {
+              // Check if number value includes the search query
+              return value
+                .toString()
+                .includes(searchQuery.replace(",", "").toLowerCase());
+            } else if (Array.isArray(value)) {
+              // Check if any element in the array includes the search query
+              return value.some((val) =>
+                val.toLowerCase().includes(searchQuery.toLowerCase())
+              );
+            } else if (value instanceof Date) {
+              // Define date formats to check
+              const formats = [
+                "dd MM yyyy",
+                "dd.MM.yyyy",
+                "dd.MM yyyy",
+                "dd MMM yyyy",
+                "dd MMMM yyyy",
+                "dd MMMM yyyy",
+              ];
+
+              // Format the date in multiple formats with `date-fns`
+              const formattedDates = formats.map((fmt) =>
+                format(value, fmt, { locale: pl })
+              );
+
+              // Check if any of the formats contains the searchQuery
+              const matches = formattedDates.some((dateStr) =>
+                dateStr.includes(searchQuery)
+              );
+
+              return matches; // Returns true if any of the formats contains searchQuery
+            }
+
+            return false;
+          });
+        });
+      };
+
+      // Define the type for fetchedData
+      type FetchedDataType =
+        | Record<string, any>[]
+        | {
+            [key: string]: Record<string, any>[];
+          };
+
+      // Cast fetchedData to the defined type
+      const data = fetchedData as FetchedDataType;
+
+      // Check if fetchedData is an array or an object
+      if (Array.isArray(data)) {
+        // Filter the array using filterData function
+        fetchedData = filterData(data);
+      } else if (typeof data === "object" && data !== null) {
+        // Iterate over keys in fetchedData if it's an object
+        for (const key in data) {
+          if (Array.isArray(data[key])) {
+            // Filter each array in the object using filterData function
+            data[key] = filterData(data[key]);
+          }
+        }
+        fetchedData = data;
+      }
+    }
+
     setData(fetchedData as Data);
   }, [
     currentSortBy,
@@ -117,6 +222,7 @@ export default function AdminContextProvider<Data extends object[]>({
     dateRangeFrom,
     dateRangeTo,
     pathname,
+    searchQuery,
   ]);
 
   useEffect(() => {
