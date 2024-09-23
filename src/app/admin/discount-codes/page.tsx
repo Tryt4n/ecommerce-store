f1896bcd26b52b9e86476bca9a31a0c10825b961
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-23 11:52:00 +0200

    Changed how `<AdminPageHeader/>` is used after changing a component.

--- src/app/admin/discount-codes/page.tsx
+++ src/app/admin/discount-codes/page.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import AdminPageHeader from "../_components/AdminPageHeader";
 import DiscountCodesSections from "./_layout/DiscountCodesSections";
-import { Button } from "@/components/ui/button";
 
 export default async function AdminDiscountCodesPage() {
   return (
     <>
-      <div className="flex items-center justify-between gap-4">
-        <AdminPageHeader>Coupons</AdminPageHeader>
-
-        <Button href="/admin/discount-codes/new">Add Coupon</Button>
-      </div>
+      <AdminPageHeader
+        withLink={{ label: "Add Coupon", href: "/admin/discount-codes/new" }}
+      >
+        Coupons
+      </AdminPageHeader>
 
       <DiscountCodesSections />
     </>
--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import AdminPageHeader from "../_components/AdminPageHeader";
 import ProductsTable from "./_components/ProductsTable";
-import { Button } from "@/components/ui/button";
 
 export default function AdminProductsPage() {
   return (
     <>
-      <div className="flex items-center justify-between gap-4">
-        <AdminPageHeader>Products</AdminPageHeader>
-        <Button href="/admin/products/new">Add Product</Button>
-      </div>
+      <AdminPageHeader
+        withLink={{ label: "Add Product", href: "/admin/products/new" }}
+      >
+        Products
+      </AdminPageHeader>
 
       <ProductsTable />
     </>
