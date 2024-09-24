ab56900a678eba30c916961f79772ec894da83d1
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-24 14:38:56 +0200

    Added `<Toaster/>` component to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
@@ -19,10 +20,11 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "relative min-h-screen bg-background font-sans antialiased",
           inter.variable
         )}
       >
+        <Toaster />
         {children}
       </body>
     </html>
