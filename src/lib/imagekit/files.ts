b592a3f5dde1ea120608929361a294f4309284c2
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-26 09:28:50 +0200

    Changed file name.

---
+++ src/lib/imagekit/files.ts
@@ -0,0 +1,97 @@
+import { encode } from "../helpers";
+import type { UploadedImage } from "./type";
+
+export async function authenticator() {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/admin/api/upload-auth`
+    );
+
+    if (!res.ok) {
+      throw new Error("Authentication failed!" + res.text());
+    }
+
+    const data = await res.json();
+    const { token, expire, signature } = data;
+
+    return { token, expire, signature };
+  } catch (error) {
+    throw new Error("Authentication failed!");
+  }
+}
+
+export async function uploadFilesToImagekit(
+  files: FileList,
+  setProgress: (progress: number) => void
+) {
+  const totalFiles = files.length;
+  let totalLoaded = 0;
+  const uploadedImages: UploadedImage[] = [];
+
+  for (let i = 0; i < totalFiles; i++) {
+    const file = files[i];
+    const { token, expire, signature } = await authenticator();
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("fileName", file.name);
+    formData.append("publicKey", process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY);
+    formData.append("signature", signature);
+    formData.append("expire", expire);
+    formData.append("token", token);
+
+    const url = "https://upload.imagekit.io/api/v1/files/upload";
+
+    await new Promise<void>((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("POST", url, true);
+
+      xhr.upload.onprogress = (event) => {
+        if (event.lengthComputable) {
+          totalLoaded += event.loaded;
+          const progress = (totalLoaded / (event.total * totalFiles)) * 100;
+          setProgress(progress);
+        }
+      };
+
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          const data = JSON.parse(xhr.responseText);
+          uploadedImages.push({ url: data.url, id: data.fileId });
+          resolve();
+        } else {
+          reject(new Error(`Upload failed: ${xhr.statusText}`));
+        }
+      };
+
+      xhr.onerror = () => {
+        reject(new Error("Upload failed!"));
+      };
+
+      xhr.send(formData);
+    });
+  }
+
+  return uploadedImages;
+}
+
+export async function deleteImageInImageKit(fileId: string) {
+  const encodedAPIKey = encode(process.env.NEXT_PUBLIC_IMAGEKIT_PRIVATE_KEY);
+
+  const url = `https://api.imagekit.io/v1/files/${fileId}`;
+  const options = {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Basic ${encodedAPIKey}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
