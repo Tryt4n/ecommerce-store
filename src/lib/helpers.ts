426aeda2b7b56d15db7cc880f641931441d0e7f0
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-26 09:28:26 +0200

    Created function for `base64` encoding.

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,7 @@
 export function wait(duration: number) {
   return new Promise((resolve) => setTimeout(resolve, duration));
 }
+
+export function encode(data: string) {
+  return Buffer.from(`${data}:`).toString("base64");
+}
