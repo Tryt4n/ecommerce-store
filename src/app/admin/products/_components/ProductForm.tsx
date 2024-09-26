c84519a9d9e163865f7c11abfd71b610cfd4103a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-26 14:47:06 +0200

    Moved images state from `<ImageUpload/>` component. Added cancel button.

--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useFormState } from "react-dom";
 import { useToast } from "@/hooks/useToast";
 import { formatCurrency } from "@/lib/formatters";
@@ -9,11 +9,13 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import SubmitButton from "@/components/SubmitButton";
+import CancelButton from "./CancelButton";
 import ErrorMessage from "@/components/ErrorMessage";
 import MultipleSelector from "@/components/ui/multiple-selector";
-import ImageUpload from "@/components/ImageUpload";
+import { ImageUpload } from "@/components/ImageUpload";
 import type { getCategories } from "@/db/userData/categories";
 import type { getProduct } from "@/db/userData/products";
+import type { UploadedImage } from "@/lib/imagekit/type";
 
 type ProductFormProps = {
   product?: Awaited<ReturnType<typeof getProduct>>;
@@ -25,13 +27,22 @@ export default function ProductForm({ product, categories }: ProductFormProps) {
     product == null ? addProduct : updateProduct.bind(null, product.id),
     {}
   );
-  const [selectedCategories, setSelectedCategories] = useState<
-    ProductFormProps["categories"]
-  >(product?.categories || []);
+  const [name, setName] = useState(product?.name || "");
   const [priceInCents, setPriceInCents] = useState<number | undefined>(
     product?.priceInCents
   );
+  const [selectedCategories, setSelectedCategories] = useState<
+    ProductFormProps["categories"]
+  >(product?.categories || []);
+  const [images, setImages] = useState<UploadedImage[]>(product?.images || []);
+
   const { toast } = useToast();
+
+  const ikUploadRef = useRef<HTMLInputElement>(null);
+
+  const MIN_NAME_LENGTH = 5;
+  const nameInputDisabledStatus =
+    name.length >= MIN_NAME_LENGTH && images.length > 0 ? true : false;
 
   return (
     <form action={action} className="space-y-8">
@@ -41,10 +52,18 @@ export default function ProductForm({ product, categories }: ProductFormProps) {
           type="text"
           name="name"
           id="name"
-          defaultValue={product?.name}
           required
-          minLength={1}
+          minLength={MIN_NAME_LENGTH}
+          value={name}
+          disabled={nameInputDisabledStatus}
+          onChange={(e) => setName(e.target.value)}
         />
+        {
+          // Display hidden input if nameInputDisabledStatus is true because `disabled` input won't be submitted
+          nameInputDisabledStatus && (
+            <Input type="hidden" name="name" value={name} />
+          )
+        }
         {error?.name && <ErrorMessage error={error.name} />}
       </div>
 
@@ -128,11 +147,26 @@ export default function ProductForm({ product, categories }: ProductFormProps) {
       </div>
 
       <div className="space-y-2">
-        <ImageUpload productImages={product?.images} />
+        <ImageUpload
+          ref={ikUploadRef}
+          allUploadedImages={images}
+          setAllUploadedImages={setImages}
+          alreadyExistingProductImages={product?.images}
+          directoryName={name}
+        />
         {error?.images && <ErrorMessage error={error.images} />}
       </div>
 
-      <SubmitButton edit={product ? true : false} />
+      <div className="flex flex-row gap-4">
+        <SubmitButton edit={product ? true : false} />
+
+        <CancelButton
+          allUploadedImages={images}
+          alreadyExistingProductImages={product?.images}
+          folderName={name}
+          canDeleteFolder={!product && images.length > 0}
+        />
+      </div>
     </form>
   );
 }
