6945c8a3a0fa8923cf55430ba4ab5f9a465aa7a1
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-09-27 16:18:09 +0200

    Added custom error messages.

--- src/lib/zod/productSchema.ts
+++ src/lib/zod/productSchema.ts
@@ -6,19 +6,25 @@ const uploadedImageSchema = z.object({
   isMainForProduct: z.boolean().default(false),
 });
 
-const fileSchema = z
-  .object({
-    id: z.string(),
-    name: z.string(),
-    url: z.string().url(),
-  })
-  .optional();
+const fileSchema = z.object({
+  id: z.string(),
+  name: z
+    .string()
+    .min(3, { message: "File name is too short. Min 3 characters." })
+    .max(255, { message: "File name is too long. Max 255 characters." }),
+  url: z.string().url(),
+});
 
 export const productAddSchema = z.object({
-  name: z.string().min(1),
-  description: z.string().min(10),
-  priceInCents: z.coerce.number().int().min(1),
-  productFile: fileSchema,
+  name: z.string().min(1, { message: "Name is required." }),
+  description: z
+    .string()
+    .min(10, { message: "Please provide a valid description." }),
+  priceInCents: z.coerce
+    .number()
+    .int({ message: "Please provide valid price." })
+    .min(1, { message: "Price can't be less than 1." }),
+  productFile: fileSchema.optional(),
   images: z
     .array(uploadedImageSchema, {
       message: "Upload at least one image for the product.",
@@ -29,5 +35,5 @@ export const productAddSchema = z.object({
       message: "You must select at least one category for the product.",
     })
     .nonempty()
-    .max(3),
+    .max(3, { message: "You can only select up to 3 categories." }),
 });
