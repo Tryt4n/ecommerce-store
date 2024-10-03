85bd09f868ad31721852f6ce17e2c1c8c1a60897
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-03 10:46:08 +0200

    Synchronized middleware with `kinde`.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,10 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { isValidPassword } from "./lib/isValidPassword";
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  if ((await isAuthenticated(req)) === false) {
-    return new NextResponse("Unauthorized", {
-      status: 401,
-      headers: { "WWW-Authenticate": "Basic" },
-    });
-  }
-}
-
-async function isAuthenticated(req: NextRequest) {
-  const authHeader =
-    req.headers.get("Authorization") || req.headers.get("authorization");
-
-  if (authHeader == null) return false;
-
-  const [username, password] = Buffer.from(authHeader.split(" ")[1], "base64") // Decode base64, [1] because the returned value is "Basic <base64>"
-    .toString()
-    .split(":"); // "username:password"
-
-  return (
-    username === process.env.ADMIN_USERNAME &&
-    (await isValidPassword(password, process.env.HASHED_ADMIN_PASSWORD))
-  );
+  return withAuth(req);
 }
 
 export const config = {
-  matcher: "/admin/:path*",
+  matcher: ["/admin/:path*", "/orders/:path"],
 };
