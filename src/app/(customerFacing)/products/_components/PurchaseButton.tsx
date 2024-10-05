ddefedb9d43c0732b732395baa56c91bda012ec9
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-05 16:28:17 +0200

    Created purchase button.

---
+++ src/app/(customerFacing)/products/_components/PurchaseButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+export default function PurchaseButton({ id }: { id: string }) {
+  const router = useRouter();
+
+  return (
+    <Button
+      size={"lg"}
+      className="w-full"
+      onClick={() => router.push(`/products/${id}/purchase`)}
+    >
+      Purchase
+    </Button>
+  );
+}
