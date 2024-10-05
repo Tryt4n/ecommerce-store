cbdabfac21fe3ba22b45993d32bc8013ac1c92e2
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-05 16:28:03 +0200

    Created products context.

---
+++ src/app/(customerFacing)/products/_context/ProductsContext.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { createContext, useState } from "react";
+import { defaultProductsLayout, type ProductsLayout } from "../_types/layout";
+
+type ProductsContextType = {
+  layout: ProductsLayout;
+  setLayout: (layout: ProductsLayout) => void;
+};
+
+export const ProductsContext = createContext<ProductsContextType | null>(null);
+
+export default function ProductsContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [layout, setLayout] = useState<ProductsLayout>(defaultProductsLayout);
+
+  return (
+    <ProductsContext.Provider value={{ layout, setLayout }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+}
