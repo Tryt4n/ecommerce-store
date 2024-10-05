ea64ed5ac3f3a99a0c6502b01c7c49bf54b74fb0
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-05 16:32:18 +0200

    Edited products page - wrapped all page into context provider, added header and footer and changed usage of all components to custom components.

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,18 +1,44 @@
 import React, { Suspense } from "react";
-import ListGrid from "@/layout/ListGrid";
+import {
+  getAllAvailableForPurchaseProducts,
+  getAllAvailableProductsCount,
+} from "@/db/userData/products";
+import ProductsContextProvider from "./_context/ProductsContext";
+import CustomLayoutListGrid from "./_components/CustomLayoutListGrid";
+import CustomProductsSuspense from "./_components/CustomProductsSuspense";
 import ProductCardSkeleton from "@/components/ProductCardSkeleton";
-import ProductSuspense from "@/components/ProductSuspense";
-import { getAllAvailableForPurchaseProducts } from "@/db/userData/products";
+import LayoutButton from "./_components/LayoutButton";
+import ProductsPerView from "./_components/ProductsPerView";
+import PageNavigation from "./_components/PageNavigation";
+import type { Product } from "@prisma/client";
+import type { SortingType } from "@/types/sort";
 
-export default function ProductsPage() {
+export type ProductsSearchParams = {
+  page?: string;
+  take?: string;
+  sortBy?: keyof Product;
+  order?: SortingType;
+};
+
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: ProductsSearchParams;
+}) {
+  const productsCount = await getAllAvailableProductsCount();
+
   return (
-    <>
+    <ProductsContextProvider>
       <h1 className="sr-only">Products Page</h1>
 
-      <section className="space-y-4">
-        <h2 className="text-3xl font-bold">Products</h2>
+      <article className="space-y-4">
+        <header className="flex justify-between">
+          <h2 className="text-3xl font-bold">Products</h2>
 
-        <ListGrid>
+          <LayoutButton />
+        </header>
+
+        <CustomLayoutListGrid>
           <Suspense
             fallback={
               <>
@@ -25,12 +51,22 @@ export default function ProductsPage() {
               </>
             }
           >
-            <ProductSuspense
+            <CustomProductsSuspense
               productsFetcher={getAllAvailableForPurchaseProducts}
+              searchParams={searchParams}
             />
           </Suspense>
-        </ListGrid>
-      </section>
-    </>
+        </CustomLayoutListGrid>
+
+        <footer className="flex flex-row items-center justify-between gap-4">
+          <PageNavigation
+            searchParams={searchParams}
+            productsCount={productsCount}
+          />
+
+          <ProductsPerView searchParams={searchParams} />
+        </footer>
+      </article>
+    </ProductsContextProvider>
   );
 }
