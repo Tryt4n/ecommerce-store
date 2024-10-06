739868cc2a69a2b4180178c8b6b0495d14885989
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-06 13:33:24 +0200

    Moved search params type to product context.

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import ProductsContextProvider from "./_context/ProductsContext";
+import ProductsContextProvider, {
+  type ProductsSearchParams,
+} from "./_context/ProductsContext";
 import ProductsHeader from "./_layout/ProductsHeader";
 import Products from "./_layout/Products";
 import ProductsFooter from "./_layout/ProductsFooter";
-import type { Product } from "@prisma/client";
-import type { SortingType } from "@/types/sort";
-
-export type ProductsSearchParams = {
-  page?: string;
-  take?: string;
-  sortBy?: keyof Product;
-  order?: SortingType;
-};
 
 export default function ProductsPage({
   searchParams,
@@ -27,7 +20,7 @@ export default function ProductsPage({
 
         <Products searchParams={searchParams} />
 
-        <ProductsFooter searchParams={searchParams} />
+        <ProductsFooter />
       </article>
     </ProductsContextProvider>
   );
