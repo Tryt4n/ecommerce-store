5a21c71b8366668f8ab652be3a76808a46994800
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-06 13:36:21 +0200

    Deleted unnecessary props.

--- src/app/(customerFacing)/products/_layout/ProductsFooter.tsx
+++ src/app/(customerFacing)/products/_layout/ProductsFooter.tsx
@@ -2,23 +2,15 @@ import React from "react";
 import { getAllAvailableProductsCount } from "@/db/userData/products";
 import PageNavigation from "../_components/PageNavigation";
 import ProductsPerView from "../_components/ProductsPerView";
-import type { ProductsSearchParams } from "../page";
 
-export default async function ProductsFooter({
-  searchParams,
-}: {
-  searchParams: ProductsSearchParams;
-}) {
+export default async function ProductsFooter() {
   const productsCount = await getAllAvailableProductsCount();
 
   return (
     <footer className="flex flex-col gap-8 sm:flex-row sm:items-end sm:justify-between">
-      <PageNavigation
-        searchParams={searchParams}
-        productsCount={productsCount}
-      />
+      <PageNavigation productsCount={productsCount} />
 
-      <ProductsPerView searchParams={searchParams} />
+      <ProductsPerView />
     </footer>
   );
 }
