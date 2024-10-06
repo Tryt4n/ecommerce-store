cd0c640310d4e6fbfb83ea482150064c0b9b9fdc
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-06 13:36:11 +0200

    Created serach params object.

--- src/app/(customerFacing)/products/_context/ProductsContext.tsx
+++ src/app/(customerFacing)/products/_context/ProductsContext.tsx
@@ -1,14 +1,25 @@
 "use client";
 
 import { createContext, useState } from "react";
+import { useSearchParams } from "next/navigation";
 import {
   defaultProductsLayout,
   type ProductsLayout,
 } from "../_types/layoutTypes";
+import type { Product } from "@prisma/client";
+import type { SortingType } from "@/types/sort";
+
+export type ProductsSearchParams = {
+  page?: string;
+  take?: string;
+  sortBy?: keyof Product;
+  order?: SortingType;
+};
 
 type ProductsContextType = {
   layout: ProductsLayout;
   setLayout: (layout: ProductsLayout) => void;
+  searchParams: ProductsSearchParams;
 };
 
 export const ProductsContext = createContext<ProductsContextType | null>(null);
@@ -20,8 +31,26 @@ export default function ProductsContextProvider({
 }) {
   const [layout, setLayout] = useState<ProductsLayout>(defaultProductsLayout);
 
+  const searchParams = useSearchParams();
+
+  const page = searchParams.get("page") as ProductsSearchParams["page"];
+  const take = searchParams.get("take") as ProductsSearchParams["take"];
+  const sortBy = searchParams.get("sortBy") as ProductsSearchParams["sortBy"];
+  const order = searchParams.get("order") as ProductsSearchParams["order"];
+
+  const contextValues = {
+    layout,
+    setLayout,
+    searchParams: {
+      page,
+      take,
+      sortBy,
+      order,
+    },
+  };
+
   return (
-    <ProductsContext.Provider value={{ layout, setLayout }}>
+    <ProductsContext.Provider value={contextValues}>
       {children}
     </ProductsContext.Provider>
   );
