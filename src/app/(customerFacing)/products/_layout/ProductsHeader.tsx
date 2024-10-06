242690a1e80b2c9732eacf276199040b33515fea
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-06 13:34:17 +0200

    Added a button to filter product order.

--- src/app/(customerFacing)/products/_layout/ProductsHeader.tsx
+++ src/app/(customerFacing)/products/_layout/ProductsHeader.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FiltersButton from "../_components/FiltersButton";
 import LayoutButton from "../_components/LayoutButton";
 
 export default function ProductsHeader() {
@@ -6,7 +7,10 @@ export default function ProductsHeader() {
     <header className="flex justify-between">
       <h2 className="text-3xl font-bold">Products</h2>
 
-      <LayoutButton />
+      <div className="flex gap-2 sm:gap-4">
+        <FiltersButton />
+        <LayoutButton />
+      </div>
     </header>
   );
 }
