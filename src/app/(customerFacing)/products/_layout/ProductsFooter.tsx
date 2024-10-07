c81f7dd7edf833c5c639f5eb5ed6d90861bbc35b
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-07 09:02:18 +0200

    Moved product count logic to product context. Deleted unnecessary passed props.

--- src/app/(customerFacing)/products/_layout/ProductsFooter.tsx
+++ src/app/(customerFacing)/products/_layout/ProductsFooter.tsx
@@ -1,34 +1,18 @@
 import React from "react";
-import { getAllAvailableProductsCount } from "@/db/userData/products";
 import PageNavigation from "../_components/PageNavigation";
 import ProductsPerView from "../_components/ProductsPerView";
-import { defaultProductsPerPage } from "../_types/layoutTypes";
 import type { ProductsSearchParams } from "../page";
 
-export default async function ProductsFooter({
+export default function ProductsFooter({
   searchParams,
 }: {
   searchParams: ProductsSearchParams;
 }) {
-  const productsCount = await getAllAvailableProductsCount();
-
-  const lastPageNumber =
-    productsCount &&
-    Math.ceil(
-      productsCount / (Number(searchParams.take) || defaultProductsPerPage)
-    );
-
   return (
     <footer className="flex flex-col gap-8 sm:flex-row sm:items-end sm:justify-between">
-      <PageNavigation
-        searchParams={searchParams}
-        lastPageNumber={lastPageNumber}
-      />
+      <PageNavigation searchParams={searchParams} />
 
-      <ProductsPerView
-        searchParams={searchParams}
-        lastPageNumber={lastPageNumber}
-      />
+      <ProductsPerView searchParams={searchParams} />
     </footer>
   );
 }
