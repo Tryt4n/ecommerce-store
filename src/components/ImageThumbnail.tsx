a9ddc6032df59cddb3b97276555e223ae784c768
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-10 10:46:10 +0200

    Created custom image card for thumbnail.

---
+++ src/components/ImageThumbnail.tsx
@@ -0,0 +1,23 @@
+import React, { type ComponentProps } from "react";
+import Image from "./Image";
+
+type ImageThumbnailProps = {
+  containerStyles?: string;
+  children?: React.ReactNode;
+} & ComponentProps<typeof Image>;
+
+export default function ImageThumbnail({
+  containerStyles,
+  children,
+  ...image
+}: ImageThumbnailProps) {
+  return (
+    <div
+      className={`relative flex aspect-square max-h-[${image.width}px] items-center justify-center overflow-hidden${containerStyles ? ` ${containerStyles}` : ""}`}
+    >
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <Image {...image} isThumbnail />
+      {children}
+    </div>
+  );
+}
