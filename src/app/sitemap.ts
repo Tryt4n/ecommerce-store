ab1038c11a378bfa2315edbdb7cfe3e16cae1dfe
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-14 10:40:22 +0200

    Created sitemap.xml file.

---
+++ src/app/sitemap.ts
@@ -0,0 +1,28 @@
+import { getAllAvailableProductsIds } from "@/db/userData/products";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const products = await getAllAvailableProductsIds();
+
+  const productEntries: MetadataRoute.Sitemap =
+    products?.flatMap((product) => [
+      {
+        url: `${process.env.NEXT_PUBLIC_SERVER_URL}/products/${product.id}`,
+        lastModified: product.updatedAt,
+      },
+      {
+        url: `${process.env.NEXT_PUBLIC_SERVER_URL}/products/purchase/${product.id}`,
+        lastModified: product.updatedAt,
+      },
+    ]) ?? [];
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_SERVER_URL}/products`,
+    },
+    ...productEntries,
+  ];
+}
