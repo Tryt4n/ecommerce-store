7c1eaa71b7658bbf2b0d3549174d5952e1878c9c
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-14 15:28:42 +0200

    Created orders page.

--- src/app/(customerFacing)/orders/page.tsx
+++ src/app/(customerFacing)/orders/page.tsx
@@ -1,57 +1,25 @@
-"use client";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { getAllUserOrders } from "@/db/userData/orders";
+import OrdersHistory from "./_components/OrdersHistory";
 
-import React from "react";
-import { useFormState } from "react-dom";
-import { emailOrdersHistory } from "@/app/_actions/order";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import ErrorMessage from "@/components/ErrorMessage";
-import SubmitButton from "@/components/SubmitButton";
+export default async function MyOrdersPage() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-export default function MyOrdersPage() {
-  const [data, action] = useFormState(emailOrdersHistory, {});
+  const ordersHistory =
+    user && user.email && (await getAllUserOrders(user.email));
 
   return (
-    <form action={action} className="max-2-xl mx-auto mt-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>My Orders</CardTitle>
-          <CardDescription>
-            Enter your email and we will email you your order history and
-            download links.
-          </CardDescription>
-        </CardHeader>
+    <>
+      <h1 className="text-balance text-4xl">Your Orders History</h1>
 
-        <CardContent>
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input type="email" name="email" id="email" required />
-
-            {data.error && <ErrorMessage error={data.error} />}
-          </div>
-        </CardContent>
-
-        <CardFooter>
-          {data.message ? (
-            <p className="text-green-500">{data.message}</p>
-          ) : (
-            <SubmitButton
-              size="lg"
-              className="w-full"
-              initialText="Send"
-              pendingText="Sending..."
-            />
-          )}
-        </CardFooter>
-      </Card>
-    </form>
+      {ordersHistory && ordersHistory.length >= 1 ? (
+        <OrdersHistory orders={ordersHistory} />
+      ) : (
+        <p className="my-8 text-pretty text-center text-base">
+          You haven&apos;t made any purchases in our store yet.
+        </p>
+      )}
+    </>
   );
 }
