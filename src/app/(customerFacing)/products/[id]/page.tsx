dc7d08085b254ba40761809a305a6d8745ff496a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-15 15:41:17 +0200

    Added carousel slider component.

--- src/app/(customerFacing)/products/[id]/page.tsx
+++ src/app/(customerFacing)/products/[id]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { notFound } from "next/navigation";
 import { getProduct } from "@/db/userData/products";
+import CarouselSlider from "./_components/CarouselSlider";
 
 export default async function ProductPage({
   params: { id },
@@ -12,9 +13,16 @@ export default async function ProductPage({
   if (product == null) notFound();
 
   return (
-    <main>
-      <h1>{product.name}</h1>
-      <p>{product.description}</p>
-    </main>
+    <div className="my-8 flex flex-col-reverse items-center gap-8 lg:flex-row-reverse [&>[data-carousel-slider]]:max-w-full lg:[&>[data-carousel-slider]]:w-[66%] lg:[&>[data-carousel-slider]]:max-w-[900px]">
+      <div>
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+      </div>
+
+      <CarouselSlider
+        productName={product.name}
+        imagesUrl={product.images.map((image) => image.url)}
+      />
+    </div>
   );
 }
