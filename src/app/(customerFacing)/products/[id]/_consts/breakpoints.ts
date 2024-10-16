8f70ba29576dcfa48f2f69ddd72dbfd81661981a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-16 11:04:07 +0200

    Created a constant with breakpoints for thumbnail images.

---
+++ src/app/(customerFacing)/products/[id]/_consts/breakpoints.ts
@@ -0,0 +1,11 @@
+export const carouselSliderThumbnailsBreakpoint = [
+  { minWidth: 1280, value: 200 },
+  { minWidth: 1024, value: 150 },
+  { minWidth: 768, value: 175 },
+  { minWidth: 600, value: 135 },
+  { minWidth: 520, value: 115 },
+  { minWidth: 480, value: 100 },
+  { minWidth: 425, value: 125 },
+  { minWidth: 350, value: 100 },
+  { minWidth: 1, value: 90 },
+] as const;
