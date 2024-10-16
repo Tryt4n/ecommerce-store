ce651da6cc18606d0b491ce9499e9ee1a63bfe2b
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-16 11:04:41 +0200

    Added button props to component.

--- src/app/(customerFacing)/products/_components/PurchaseButton.tsx
+++ src/app/(customerFacing)/products/_components/PurchaseButton.tsx
@@ -1,10 +1,14 @@
 "use client";
 
-import React from "react";
+import React, { type ComponentProps } from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 
-export default function PurchaseButton({ id }: { id: string }) {
+type PurchaseButtonProps = {
+  id: string;
+} & ComponentProps<typeof Button>;
+
+export default function PurchaseButton({ id, ...props }: PurchaseButtonProps) {
   const router = useRouter();
 
   return (
@@ -12,6 +16,7 @@ export default function PurchaseButton({ id }: { id: string }) {
       size={"lg"}
       className="w-full"
       onClick={() => router.push(`/products/purchase/${id}`)}
+      {...props}
     >
       Purchase
     </Button>
