5d4323d0ed43848b364f4499be3e637acf21215c
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-16 11:05:02 +0200

    Created product buy module component.

---
+++ src/app/(customerFacing)/products/[id]/_components/ProductBuyModule.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { formatCurrency } from "@/lib/formatters";
+import PurchaseButton from "../../_components/PurchaseButton";
+import type { getProduct } from "@/db/userData/products";
+
+type ProductBuyModuleProps = {
+  product: NonNullable<Awaited<ReturnType<typeof getProduct>>>;
+};
+
+export default function ProductBuyModule({ product }: ProductBuyModuleProps) {
+  return (
+    <div className="w-full">
+      <hgroup className="my-4">
+        <h1 className="text-balance text-2xl font-medium">{product.name}</h1>
+        <p className="mb-1 text-sm capitalize text-muted-foreground">
+          {`${product.categories.length > 1 ? "Categories" : "Category"}: ${product.categories.join(" / ")}`}
+        </p>
+        <p className="text-xl font-medium">
+          {formatCurrency(product.priceInCents / 100)}
+        </p>
+        <p className="my-2 text-pretty">{product.description}</p>
+      </hgroup>
+
+      <PurchaseButton id={product.id} className="w-full" />
+    </div>
+  );
+}
