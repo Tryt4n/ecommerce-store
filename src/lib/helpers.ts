52a402dd2283825904ec1e124bafb6f7d89b9d07
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-17 09:06:04 +0200

    Added function to decode data.

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -5,3 +5,7 @@ export function wait(duration: number) {
 export function encode(data: string) {
   return Buffer.from(`${data}:`).toString("base64");
 }
+
+export function decode(data: string) {
+  return Buffer.from(data, "base64").toString().slice(0, -1);
+}
