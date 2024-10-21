1ef27b30c040f6978f224aae0f03fc94d80807dd
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-21 13:27:56 +0200

    Created action to finalize purchase process.

---
+++ src/app/(customerFacing)/purchase/_actions/purchaseProducts.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import { createOrEditUserWithOrder } from "@/db/userData/user";
+import {
+  createStripeCheckoutSession,
+  createStripeCustomer,
+} from "@/lib/stripe/stripe";
+import { redirect } from "next/navigation";
+
+export async function handlePurchaseProduct(
+  products: {
+    productId: string;
+    quantity: number;
+    priceInCents: number;
+  }[],
+  prevState: unknown,
+  formData: FormData
+) {
+  const email = formData.get("email") as string;
+  const firstName = formData.get("firstName") as string;
+  const lastName = formData.get("lastName") as string;
+
+  if (products.length === 0) {
+    return "No products to purchase.";
+  }
+  if (products.length > 20) {
+    return "Maximum of 20 products can be purchased at once.";
+  }
+  if (!email || email === "") {
+    return "Email is required.";
+  }
+
+  // Create a new customer in Stripe - if the customer already exists, it will return the existing customer
+  const customer = await createStripeCustomer({
+    email: email,
+    name:
+      firstName || lastName
+        ? `${firstName ? firstName : ""} ${lastName ? lastName : ""}`
+        : undefined,
+  });
+
+  if (!customer) return "Failed to create Stripe customer.";
+
+  const orderId = crypto.randomUUID();
+
+  const user = await createOrEditUserWithOrder(
+    customer.id,
+    customer.email!,
+    products,
+    orderId
+  );
+
+  if (!user) return "Failed to create user.";
+
+  // If the user was created successfully, create a new checkout session
+  const checkoutSession = await createStripeCheckoutSession(
+    user.id,
+    products,
+    orderId
+  );
+
+  if (checkoutSession && checkoutSession.url) {
+    redirect(checkoutSession.url);
+  }
+
+  return checkoutSession;
+}
