4a138fce63848179d658d6778eb4c2486b812510
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-21 13:25:40 +0200

    Created placeholder page.

--- src/app/(customerFacing)/purchase/page.tsx
+++ src/app/(customerFacing)/purchase/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ShoppingCart from "./_components/ShoppingCart";
 
-export default function PurchasePage() {
+export default async function PurchasePage() {
   return (
     <div>
       <h1>Purchase</h1>
+
+      <ShoppingCart />
     </div>
   );
 }
---
+++ src/app/(customerFacing)/purchase/success/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PurchaseSuccessPage() {
+  return <h1>PurchaseSuccessPage</h1>;
+}
