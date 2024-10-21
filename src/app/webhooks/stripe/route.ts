52b7230e6f01fb76d690e93535039cbb12a1bb1f
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-21 13:25:22 +0200

    Updated `stripe` webhook to use `checkout.session.completed` type instead of `charge.succeeded`.

--- src/app/webhooks/stripe/route.ts
+++ src/app/webhooks/stripe/route.ts
@@ -1,32 +1,58 @@
 import Stripe from "stripe";
 import { NextResponse, type NextRequest } from "next/server";
-import { getProduct } from "@/db/userData/products";
-import { createOrEditUser } from "@/db/userData/user";
+import db from "@/db/init";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req: NextRequest) {
-  const event = stripe.webhooks.constructEvent(
-    await req.text(),
-    req.headers.get("stripe-signature") as string,
-    process.env.STRIPE_WEBHOOK_SECRET
-  );
+  try {
+    const event = stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
 
-  if (event.type === "charge.succeeded") {
-    const charge = event.data.object;
-    const productId = charge.metadata.productId;
-    const discountCodeId = charge.metadata.discountCodeId;
-    const email = charge.billing_details.email;
-    const pricePaidInCents = charge.amount;
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+      const orderId = session.metadata?.orderIdInDB;
 
-    const product = await getProduct(productId);
+      if (!orderId) {
+        return NextResponse.json(
+          { error: "No orderIdInDB in metadata" },
+          { status: 400 }
+        );
+      }
 
-    if (!product || email == null) {
-      return new NextResponse("Bad Request", { status: 400 });
+      try {
+        await db.order.update({
+          where: { id: orderId },
+          data: { isPaid: true },
+        });
+      } catch (error) {
+        console.error(`Failed to update order. Error: ${error}`);
+        return NextResponse.json(
+          { error: `Failed to update order: ${error}` },
+          { status: 500 }
+        );
+      }
+
+      // TODO: Create invoice email template with `invoice.url` and send it to the user
+      // // Get Invoice
+      // const invoice = await stripe.invoices.search({
+      //   query: `metadata["orderIdInDB"]:"${orderId}"`,
+      //   limit: 1,
+      // });
+
+      // // Send Invoice
+      // invoice && (await stripe.invoices.sendInvoice(invoice.data[0].id));
     }
 
-    await createOrEditUser(email, product, pricePaidInCents, discountCodeId);
+    return NextResponse.json({ received: true }, { status: 200 });
+  } catch (error: any) {
+    console.error("Error processing webhook:", error);
+    return NextResponse.json(
+      { error: `Webhook Error: ${error.message}` },
+      { status: 400 }
+    );
   }
-
-  return new NextResponse();
 }
