93359094c7f0200b45e27293259a358c38cb87d7
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-28 14:13:06 +0100

    Finished shopping cart component.

--- src/app/(customerFacing)/purchase/_components/ShoppingCart.tsx
+++ src/app/(customerFacing)/purchase/_components/ShoppingCart.tsx
@@ -1,9 +1,13 @@
 "use client";
 
 import React from "react";
-import PurchaseForm from "./PurchaseForm";
 import { useShoppingCart } from "@/app/_hooks/useShoppingCart";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { formatCurrency } from "@/lib/formatters";
+import ShoppingCartList from "@/components/ShoppingCartList";
+import PurchaseForm from "./PurchaseForm";
+import { Separator } from "@/components/ui/separator";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 export default function ShoppingCart() {
   const { shoppingCart } = useShoppingCart();
@@ -19,10 +23,36 @@ export default function ShoppingCart() {
 
   return (
     <>
-      {user && shoppingCart && products && products?.length > 0 ? (
-        <PurchaseForm user={user} products={products} />
+      {user ? (
+        <>
+          {shoppingCart && products && products?.length > 0 ? (
+            <article>
+              <h2 className="sr-only">Shopping Cart List</h2>
+
+              <Separator className="mb-2 mt-8" />
+              <ShoppingCartList />
+              <Separator className="mb-8 mt-2" />
+
+              <p className="my-4 text-end text-base font-medium">
+                Total:&nbsp;
+                {formatCurrency(
+                  shoppingCart.reduce(
+                    (acc, item) => acc + item.priceInCents * item.quantity,
+                    0
+                  ) / 100
+                )}
+              </p>
+
+              <PurchaseForm user={user} products={products} />
+            </article>
+          ) : (
+            <p className="my-4 text-center text-base">
+              Shopping cart is empty.
+            </p>
+          )}
+        </>
       ) : (
-        <p>Shopping cart is empty.</p>
+        <LoadingSpinner size={48} />
       )}
     </>
   );
