5d62b2c07937ad98703149ff633740fd04ed5f8a
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-28 11:01:27 +0100

    Edited component.

--- src/lib/resend/templates/PurchaseReceiptEmail.tsx
+++ src/lib/resend/templates/PurchaseReceiptEmail.tsx
@@ -1,53 +1,66 @@
 import React from "react";
 import {
   Body,
+  Button,
   Container,
   Head,
   Heading,
   Html,
+  Img,
   Tailwind,
 } from "@react-email/components";
-import OrderInformation from "./OrderInformation";
-import type { Order } from "../types";
-import type { getProduct } from "@/db/userData/products";
+import OrderInformation from "../components/OrderInformation";
+import type { updateOrder } from "@/db/adminData/orders";
 
 export type PurchaseReceiptEmailProps = {
-  product: Required<
-    Pick<
-      NonNullable<Awaited<ReturnType<typeof getProduct>>>,
-      "name" | "description"
-    >
-  > & {
-    images: Pick<
-      NonNullable<Awaited<ReturnType<typeof getProduct>>>["images"][number],
-      "id" | "url"
-    >[];
-  };
-  order: Order;
+  order: NonNullable<Awaited<ReturnType<typeof updateOrder>>>;
 };
 
 // Only for preview purposes
 PurchaseReceiptEmail.PreviewProps = {
-  product: {
-    name: "Product Name",
-    images: [
-      {
-        id: "1",
-        url: "/products/9a4116b0-82e7-46ac-b2a7-2cd2b318af80-05 - Metadata.jpg",
-      },
-    ],
-    description: "Product Description",
-  },
   order: {
     id: crypto.randomUUID(),
+    receiptUrl: "https://example.com/receipt",
     createdAt: new Date(),
-    pricePaidInCents: 10000,
+    pricePaidInCents: 37997,
+    user: { email: "test@test.com" },
+    orderItems: [
+      {
+        id: "1",
+        product: {
+          name: "First Product",
+          priceInCents: 9499,
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, magni fugiat minima laboriosam obcaecati placeat est dicta eveniet dolore ratione pariatur cumque adipisci sapiente facere error impedit nihil quod? Placeat repudiandae repellendus in.",
+          images: [
+            {
+              url: "https://ik.imagekit.io/tfmnhfl9v/Products/Wireless_Security_Doorbell/blink-video-doorbell-czarny_131490944921_5__6__ZnNtOjvKj.webp?updatedAt=1728724366604",
+            },
+          ],
+        },
+        quantity: 2,
+      },
+      {
+        id: "2",
+        product: {
+          name: "Second Product",
+          priceInCents: 18999,
+          description:
+            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis, magni fugiat minima laboriosam obcaecati placeat est dicta eveniet dolore ratione pariatur cumque adipisci sapiente facere error impedit nihil quod? Placeat repudiandae repellendus in.",
+          images: [
+            {
+              url: "https://ik.imagekit.io/tfmnhfl9v/Products/Ergonomic_Office_Chair/Fotel-DIABLO-CHAIRS-V-Light-Czarny-tyl_i0Y8maFkX.jpg?updatedAt=1728724313297",
+            },
+          ],
+        },
+        quantity: 1,
+      },
+    ],
   },
 } satisfies PurchaseReceiptEmailProps;
 // Only for preview purposes
 
 export default function PurchaseReceiptEmail({
-  product,
   order,
 }: PurchaseReceiptEmailProps) {
   return (
@@ -57,10 +70,27 @@ export default function PurchaseReceiptEmail({
 
         <Body className="bg-white font-sans">
           <Container className="max-w-xl">
-            <Heading className="text-lg font-medium">Purchase Receipt</Heading>
+            <Heading className="mb-0 flex justify-between text-lg font-medium">
+              <span>Purchase Receipt</span>
+              <Img
+                src="https://ik.imagekit.io/tfmnhfl9v/Logo/logo.svg"
+                alt="E-commerce store logo"
+                width="50"
+                height="50"
+              />
+            </Heading>
 
-            <OrderInformation product={product} order={order} />
+            <OrderInformation order={order} />
           </Container>
+
+          <div className="flex flex-row justify-center">
+            <Button
+              href={`${order.receiptUrl}`}
+              className="mx-auto mb-4 mt-8 rounded bg-black px-6 py-4 text-lg text-white"
+            >
+              Show Receipt
+            </Button>
+          </div>
         </Body>
       </Tailwind>
     </Html>
