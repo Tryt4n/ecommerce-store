c144ed31522a5df701e4fb248757e8a510920f67
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2024-10-28 14:11:27 +0100

    Deleted unnecessary `createStripePaymentIntent` function.

--- src/lib/stripe/stripe.ts
+++ src/lib/stripe/stripe.ts
@@ -1,24 +1,8 @@
 "use server";
 
 import Stripe from "stripe";
-import type { MetadataParam } from "@stripe/stripe-js";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-export async function createStripePaymentIntent(
-  amount: number,
-  metadata: MetadataParam
-) {
-  try {
-    return await stripe.paymentIntents.create({
-      amount,
-      currency: "PLN",
-      metadata,
-    });
-  } catch (error) {
-    console.error(`Stripe failed to create payment intent. Error: ${error}`);
-  }
-}
 
 export async function createOrGetExistingStripeCustomer(
   params: Stripe.CustomerCreateParams,
